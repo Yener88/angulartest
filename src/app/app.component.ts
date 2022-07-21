24a284e93553076d0591e4256604be0a60ef6927
Yener88 <106224876+Yener88@users.noreply.github.com>
2022-07-21 12:36:11 +0200

    first commit 1.1

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,33 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'testangular';
+export class AppComponent implements OnInit {
+  images = ['1.jpg', '2.jpg', '3.jpg'];
+  currentImage = 0;
+  showImage = true;
+
+
+  ngOnInit() {
+    this.updateImage();
+  }
+
+  updateImage() {
+
+    setInterval(() => {
+      this.currentImage++;
+      this.currentImage = this.currentImage % this.images.length;
+      this.showImage = false;
+
+      setTimeout(() => {
+        this.showImage = true;
+      }, 10);
+    }, 8000);
+
+  }
+
+
 }
